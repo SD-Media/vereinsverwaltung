4c52bc937ce23bdca4b1135acf7ad947f7951a97
SD-Media <sd-media@t-online.de>
2026-07-17 16:25:28 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
  * Vereinsverwaltung – Laufzeitkonfiguration
  *
  * Der Mandant wird vorrangig aus ?tenant=... gelesen.
- * Ohne URL-Parameter wird vorübergehend „waldkindergarten“ verwendet.
+ * Ohne URL-Parameter wird „testverein“ verwendet.
  */
 
 export const APP_CONFIG = Object.freeze({
@@ -10,7 +10,7 @@ export const APP_CONFIG = Object.freeze({
   version: '2.0.0-alpha.1',
   apiUrl:
     'https://script.google.com/macros/s/AKfycbx0a5cuj_fOSzA4HyLlilb1u7C4xZWdUj_KKL3liKgXIXrObYDKeNRak4pz1l7HC_eS/exec',
-  defaultTenant: 'waldkindergarten',
+  defaultTenant: 'testverein',
   requestTimeoutMs: 20000
 });
 
